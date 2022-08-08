c41130c6b59154d566d5a84fd2f8ba86553de27d
r-baet <110405297+r-baet@users.noreply.github.com>
2022-08-08 20:21:11 +0200

    Added actual program

--- training/easy/ghost-legs/fsharp/src/Program.fs
+++ training/easy/ghost-legs/fsharp/src/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open OutputGenerator
+
+let token = (Console.In.ReadLine()).Split [|' '|]
+let W = int(token.[0])
+let H = int(token.[1])
+// Commented out in favor of more F# logic
+//for i in 0 .. H - 1 do
+//    let line = Console.In.ReadLine()
+//    ()
+let diagram = Array.init H (fun _ -> Console.In.ReadLine())
+
+let output = generate (String.Join(' ', token)) diagram
+    
+output
+|> Array.iter (fun s -> printfn "%s" s)

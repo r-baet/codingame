e19bd9ba570b3a473e25e37cca02d59a81ca28c2
r-baet <110405297+r-baet@users.noreply.github.com>
2022-08-12 11:49:50 +0200

    Finalized CsvData attribute

--- training/easy/the-descent/csharp/test/Attributes/CsvDataAttribute.cs
+++ training/easy/the-descent/csharp/test/Attributes/CsvDataAttribute.cs
@@ -10,21 +10,65 @@ namespace csharp.tests.Attributes
 {
     internal class CsvDataAttribute : DataAttribute
     {
-        public CsvDataAttribute(string fileName, bool hasHeaders = false)
-        {
-            FileName = fileName;
-            HasHeaders = hasHeaders;
-        }
+        private readonly string _fileName;
+        private readonly char _delimiter;
+        private readonly bool _hasHeaders;
 
-        public string FileName { get; }
-        public bool HasHeaders { get; }
+        public CsvDataAttribute(string fileName, string? directoryName = null, char delimiter = ';', bool hasHeaders = false)
+        {
+            if (fileName is null)
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+            _fileName = fileName;
+            if (!fileName.EndsWith(".csv"))
+            {
+                _fileName = _fileName + ".csv";
+            }
+            if (directoryName is not null)
+            {
+                _fileName = directoryName + "\\" + _fileName;
+            }
+            _delimiter = delimiter;
+            _hasHeaders = hasHeaders;            
+        }
 
         public override IEnumerable<object[]> GetData(MethodInfo testMethod) 
         {
-            return new List<object[]>
+            var parameterTypes = testMethod.GetParameters()?.Select(p => p.ParameterType).ToArray();
+            var result = new List<object[]>();
+            using (var csvFile = new StreamReader(_fileName))
             {
-                new object[] { 0, 9, 8, 7, 6, 5, 4, 3, 2 }
-            };
+                if (_hasHeaders)
+                {
+                    csvFile.ReadLine();
+                }
+                string? line;
+                while (!String.IsNullOrEmpty(line = csvFile.ReadLine()))
+                {
+                    var row = line.Split(_delimiter);
+                    yield return ConvertParameters(row, parameterTypes);
+                }
+            }
+        }
+        private static object[] ConvertParameters(IReadOnlyList<object> values, IReadOnlyList<Type>? parameterTypes)
+        {
+            if (parameterTypes is null)
+            {
+                throw new ArgumentNullException(nameof(parameterTypes));
+            }
+            var result = new object[parameterTypes.Count];
+            for (var idx = 0; idx < parameterTypes.Count; idx++)
+            {
+                result[idx] = ConvertParameter(values[idx], parameterTypes[idx]);
+            }
+
+            return result;
+        }
+
+        private static object ConvertParameter(object parameter, Type parameterType)
+        {
+            return parameterType == typeof(int) ? Convert.ToInt32(parameter) : parameter;
         }
     }
 }
--- training/easy/the-descent/csharp/test/TestCases.cs
+++ training/easy/the-descent/csharp/test/TestCases.cs
@@ -6,16 +6,16 @@ namespace csharp.tests;
 public class TestCases
 {
     [Theory]
-    [CsvData(fileName: nameof(Case_01_DescendingMountains), hasHeaders: true)]
-    public void Case_01_DescendingMountains(int expected, params int[] heights)
+    [CsvData(nameof(Case_01_DescendingMountains), "Input", hasHeaders: true)]
+    public void Case_01_DescendingMountains(int expected, int height0, int height1, int height2, int height3, int height4, int height5, int height6, int height7)
     {
         // Arrange
 
         // Act
-        var output = OutputGenerator.Generate(heights);
+        var output = OutputGenerator.Generate(height0, height1, height2, height3, height4, height5, height6, height7);
 
         // Asssert
-        int.Parse(output).Should().BeInRange(0, 7, "there are only eight mountains");
-        output.Should().Be("" + expected);
+        int.Parse(output).Should().BeInRange(-1, 7, "there are only eight mountains")
+            .And.Be(expected);
     }
 }

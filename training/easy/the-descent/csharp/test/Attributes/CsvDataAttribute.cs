429c3634f51c6fc8c4b4a5f550a8c70e9b35e5ad
r-baet <110405297+r-baet@users.noreply.github.com>
2022-08-12 10:31:26 +0200

    Program and test case added

---
+++ training/easy/the-descent/csharp/test/Attributes/CsvDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace csharp.tests.Attributes
+{
+    internal class CsvDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod) => throw new NotImplementedException();
+    }
+}
--- training/easy/the-descent/csharp/test/TestCases.cs
+++ training/easy/the-descent/csharp/test/TestCases.cs
@@ -1,10 +1,25 @@
+using csharp.tests.Attributes;
+using FluentAssertions;
+
 namespace csharp.tests;
 
 public class TestCases
 {
-    [Fact]
-    public void Case_01_DescendingMountains()
+    [Theory]
+    [CsvData]
+    public void Case_01_DescendingMountains(
+        int height0, int height1, int height2,
+        int height3, int height4, int height5,
+        int height6, int height7, int expected
+        )
     {
+        // Arrange
 
+        // Act
+        var output = OutputGenerator.Generate(height0, height1, height2, height3, height4, height5, height6, height7);
+
+        // Asssert
+        output.Should().BeAssignableTo(typeof(int))
+            .And.Be("" + expected);
     }
 }

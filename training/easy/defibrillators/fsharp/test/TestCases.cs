ffe0603ec091b1d1daa4cea1dec0c11979e0fd17
r-baet <110405297+r-baet@users.noreply.github.com>
2022-08-18 17:55:56 +0200

    Added rest of the test cases

--- training/easy/defibrillators/fsharp/test/TestCases.cs
+++ training/easy/defibrillators/fsharp/test/TestCases.cs
@@ -17,4 +17,43 @@ public class TestCases
         // Assert
         output.Should().Be("Maison de la Prevention Sante");
     }
+
+    [Theory]
+    [FileData(directoryName: "Input")]
+    public void Case_02_ExactPosition(string longitude, string latitude, int nrOfDefibrillators, params string[] defibrillators)
+    {
+        // Arrange
+
+        // Act
+        var output = OutputGenerator.generate(longitude, latitude, nrOfDefibrillators, defibrillators);
+
+        // Assert
+        output.Should().Be("Cimetiere Saint-Etienne");
+    }
+
+    [Theory]
+    [FileData(directoryName: "Input")]
+    public void Case_03_CompleteFile(string longitude, string latitude, int nrOfDefibrillators, params string[] defibrillators)
+    {
+        // Arrange
+
+        // Act
+        var output = OutputGenerator.generate(longitude, latitude, nrOfDefibrillators, defibrillators);
+
+        // Assert
+        output.Should().Be("Caisse Primaire d'Assurance Maladie");
+    }
+
+    [Theory]
+    [FileData(directoryName: "Input")]
+    public void Case_04_CompleteFile2(string longitude, string latitude, int nrOfDefibrillators, params string[] defibrillators)
+    {
+        // Arrange
+
+        // Act
+        var output = OutputGenerator.generate(longitude, latitude, nrOfDefibrillators, defibrillators);
+
+        // Assert
+        output.Should().Be("Amphitheatre d'O");
+    }
 }
